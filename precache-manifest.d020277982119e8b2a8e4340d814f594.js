8d6dc82ac24271ade443970b10df40e909c4c63f
sdm14 <149sdm10@gmail.com>
2020-08-20 02:37:04 +0600

    Updates

---
+++ precache-manifest.d020277982119e8b2a8e4340d814f594.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a55972c36d7f009183f0f77b3c5df5",
+    "url": "/gameselect/index.html"
+  },
+  {
+    "revision": "27bc8a486e7db743e24b",
+    "url": "/gameselect/static/css/main.13bcac74.chunk.css"
+  },
+  {
+    "revision": "64f2e57053bca21c7a07",
+    "url": "/gameselect/static/js/2.ac33ad39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gameselect/static/js/2.ac33ad39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27bc8a486e7db743e24b",
+    "url": "/gameselect/static/js/main.007ef305.chunk.js"
+  },
+  {
+    "revision": "c471903ec3155d229e36",
+    "url": "/gameselect/static/js/runtime-main.1c4b4e1c.js"
+  },
+  {
+    "revision": "529366d08734212d33bab38b02fb7b95",
+    "url": "/gameselect/static/media/ari.529366d0.png"
+  },
+  {
+    "revision": "cbaea9eca63ea05282d143fcf23f38e1",
+    "url": "/gameselect/static/media/bg.cbaea9ec.jpg"
+  },
+  {
+    "revision": "194ac6a6214682f937b4a552053c1a9b",
+    "url": "/gameselect/static/media/gazinski.194ac6a6.png"
+  },
+  {
+    "revision": "00d150439c59f188201b860389be0329",
+    "url": "/gameselect/static/media/kabella.00d15043.png"
+  },
+  {
+    "revision": "243915a03fd599aa53509e05b0dc6dca",
+    "url": "/gameselect/static/media/klasson.243915a0.png"
+  },
+  {
+    "revision": "5f6bf06482335e1e2bc94fd5ec901ede",
+    "url": "/gameselect/static/media/logo.5f6bf064.png"
+  },
+  {
+    "revision": "93a8d54fb87bb9aaf8f96f5feec9dd3e",
+    "url": "/gameselect/static/media/safonov.93a8d54f.png"
+  },
+  {
+    "revision": "e630fe0d9a336e5d1d85da5c7d5f69d6",
+    "url": "/gameselect/static/media/shapi.e630fe0d.png"
+  },
+  {
+    "revision": "4714fd65940b0633583c63be7a555b98",
+    "url": "/gameselect/static/media/utkin.4714fd65.png"
+  },
+  {
+    "revision": "9288ce072c9323597d6c90a775a926e3",
+    "url": "/gameselect/static/media/wanderson.9288ce07.png"
+  }
+]);
